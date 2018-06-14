0dab218c537bae2e6cfd50c170d1b678ab9cbf32
stephendeanharper <stephendeanharper@gmail.com>
2018-06-13 23:12:48 -0700

    Finished up the penguin classes. All connected.

--- ZooApp/ZooApp/Classes/EmperorPenguin.cs
+++ ZooApp/ZooApp/Classes/EmperorPenguin.cs
@@ -4,7 +4,20 @@ using System.Text;
 
 namespace ZooApp.Classes
 {
-    class EmperorPenguin
+    class EmperorPenguin : Penguin
     {
+        public override bool CanSwim { get; set; } = true;
+
+        public override string Diet()
+        {
+            string dd = "Big fishies";
+            return dd;
+        }
+
+        public override string Sound()
+        {
+            string ss = "Big squawks.";
+            return ss;
+        }
     }
 }
--- ZooApp/ZooApp/Classes/Penguin.cs
+++ ZooApp/ZooApp/Classes/Penguin.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ZooApp.Interfaces;
 
 namespace ZooApp.Classes
 {
-    public abstract class Penguin : Flightless
+    public abstract class Penguin : Flightless, ICanSwim
     {
+        public abstract bool CanSwim { get; set; }
+
         public virtual bool NoChinStrap()
         {
             return false;
